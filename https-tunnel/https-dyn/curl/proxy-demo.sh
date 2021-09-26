41ed5e015b55f0082f86329d196e3e8287a87c2b
lonjaju <635354870@qq.com>
2021-09-26 11:45:18 +0800

    remove https-tunnel proxy demo

--- https-tunnel/https-dyn/curl/proxy-demo.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# 通过隧道请求目标URL
-curl -x "https://https-dyn.abuyun.com:9020" --proxy-basic --proxy-user H01234567890123D:0123456789012345 http://test.abuyun.com
--- https-tunnel/https-dyn/java/httpclient/4.4+/compile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-javac -cp "./lib/*" HttpClient4xProxyDemo.java
-
-java -cp "./lib/*:." HttpClient4xProxyDemo
